1eece4b2ad309bdd4b8c8c9e8d8d2f7bbefcca47
webiner2 <118447798+webiner2@users.noreply.github.com>
2022-11-17 04:47:15 -0800

    added project

---
+++ homepage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1> welcome</h1>
+    </body>
+</html>
+<?php
+$chat = $_GET["chat"];
+echo $chat;
+?>
---
+++ uploadfile.php
@@ -0,0 +1,18 @@
+
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file1"]["file1"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit1"])) {
+  $check = getimagesize($_FILES["file1"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+?>
